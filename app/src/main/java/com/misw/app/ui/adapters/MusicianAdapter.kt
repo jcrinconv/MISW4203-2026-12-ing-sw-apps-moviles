6df9378bba3d910beec5031377b4b653645b075f
vanessaZunigaL <b.zuniga@uniandes.edu.co>
2026-04-29 23:08:43 -0500

    Add shimmer effect and image loading handling to MusicianAdapter

--- app/src/main/java/com/misw/app/ui/adapters/MusicianAdapter.kt
+++ app/src/main/java/com/misw/app/ui/adapters/MusicianAdapter.kt
@@ -1,9 +1,15 @@
 package com.misw.app.ui.adapters
 
+import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
+import com.misw.app.R
 import com.misw.app.databinding.ItemMusicianBinding
 import com.misw.app.model.Musician
 
@@ -22,8 +28,38 @@ class MusicianAdapter(private val onMusicianClick: (Int) -> Unit) :
 
         fun bind(musician: Musician) {
             binding.tvMusicianName.text = musician.name
-            Glide.with(binding.ivArtistImage.context)
+            
+            binding.shimmerLayout.startShimmer()
+
+            Glide.with(binding.root.context)
                 .load(musician.image)
+                .centerCrop()
+                .placeholder(R.drawable.ic_artists)
+                .error(R.drawable.ic_artists) // Imagen por defecto si falla la carga o permisos
+                .listener(object : RequestListener<Drawable> {
+                    override fun onResourceReady(
+                        resource: Drawable,
+                        model: Any,
+                        target: Target<Drawable>?,
+                        dataSource: DataSource,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        binding.shimmerLayout.stopShimmer()
+                        binding.shimmerLayout.hideShimmer()
+                        return false
+                    }
+
+                    override fun onLoadFailed(
+                        e: GlideException?,
+                        model: Any?,
+                        target: Target<Drawable>,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        binding.shimmerLayout.stopShimmer()
+                        binding.shimmerLayout.hideShimmer()
+                        return false
+                    }
+                })
                 .into(binding.ivArtistImage)
             
             binding.root.setOnClickListener {
