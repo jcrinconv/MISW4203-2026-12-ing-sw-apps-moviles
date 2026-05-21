288cbf7b945d07afff1a4483830add9dd5fe0680
vanessaZunigaL <b.zuniga@uniandes.edu.co>
2026-05-20 20:58:09 -0500

    Apply button bar styles to album creation layout and refactor AlbumViewModel and tests.

--- app/src/androidTest/java/com/misw/app/AlbumCreateTest.kt
+++ app/src/androidTest/java/com/misw/app/AlbumCreateTest.kt
@@ -9,7 +9,6 @@ import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
-import androidx.test.platform.app.InstrumentationRegistry
 import com.github.javafaker.Faker
 import com.misw.app.network.CacheManager
 import com.misw.app.network.EspressoIdlingResource
@@ -45,7 +44,7 @@ class AlbumCreateTest {
         RetrofitClient.setBaseUrl(mockWebServer.url("/").toString())
         IdlingRegistry.getInstance().register(EspressoIdlingResource.countingIdlingResource)
         
-        CacheManager.getInstance(InstrumentationRegistry.getInstrumentation().targetContext).clearCache()
+        CacheManager.getInstance().clearCache()
 
         val dispatcher = object : Dispatcher() {
             override fun dispatch(request: RecordedRequest): MockResponse {
